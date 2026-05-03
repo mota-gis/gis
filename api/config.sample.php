9b0e9a1fe056123fb3b670b00863a1c06efa5a7c
mota-gis <muradazh@gmail.com>
2026-05-03 19:40:20 +0300

    Add files via upload

---
+++ api/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+// انسخ هذا الملف إلى config.php وضع المفتاح هنا فقط على الخادم، ولا تضعه داخل app.js.
+return [
+  'OPENAI_API_KEY' => 'ضع_مفتاح_OpenAI_API_هنا',
+  'OPENAI_MODEL' => 'gpt-5.5'
+];
